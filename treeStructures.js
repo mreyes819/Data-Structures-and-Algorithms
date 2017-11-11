51a8c27590f4a3e9d45b2bae739a9fe9e3044422
matt <mttrys@gmail.com>
2017-11-10 16:52:55 -0800

    feat: define tree, nonBinaryTree and Binary tree

---
+++ treeStructures.js
@@ -0,0 +1,32 @@
+// non binary tree
+tree = {
+  value: null,
+  children: []
+};
+
+// binary tree
+tree = {
+  value: null,
+  left: null,
+  right: null
+}
+
+// binary search tree
+
+tree = {
+  value: 10,
+  left: {
+    value: 5,
+    left: {
+      value: 1,
+      left: null,
+      right: null
+    },
+    right: null
+  },
+  right: {
+    value: 15,
+    left: null,
+    right: null
+  }
+}
