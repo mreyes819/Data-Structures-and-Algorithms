3d3baac825d8390f1199665bd35d3c849433800e
matt <mttrys@gmail.com>
2017-11-26 20:58:32 -0800

    refactor: comment out 3rd level

--- trees/tree.js
+++ trees/tree.js
@@ -1,21 +1,27 @@
 module.exports.tree = {
   value: 3,
-  children: [
-    {
-      value: 2,
+  children: [{
+    value: 1,
+    children: [{
+      value: 'a',
+      children: []
+    }]
+  }, {
+    value: 2,
+    children: [{
+      value: 'b',
       children: []
     }, {
-      value: 1,
-      children: [{
-        value: 'a',
-        children: []
-      }, {
-        value: 'b',
-        children: []
-      }, {
-        value: 'c',
-        children: []
-      }]
-    }
-  ]
+      value: 'c',
+      children: [
+      // {
+      //   value: '1a',
+      //   children: []
+      // }, {
+      //   value: '2b',
+      //   children: []
+      // }
+      ]
+    }]
+  }]
 };
