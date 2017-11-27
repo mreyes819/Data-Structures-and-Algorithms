e1d8400f0677d326cb6dc724bd47466df9137027
matt <mttrys@gmail.com>
2017-11-27 13:44:18 -0800

    chore: house keeping. Files not in use

--- domTree.js
+++
@@ -1,6 +0,0 @@
-var rootElement = document.documentElement;
-var firstTier = rootElement.childNodes;
-
-console.log(firstTier)
-
-// firstTier.forEach(child => console.log('child', child.childNodes))
--- getMaxTreeBranchSum.js
+++
@@ -1,8 +0,0 @@
-var exports = module.exports = {}
-
-exports.getMaxTreeBranchSum = function (node) {
-  if (!node) return 0;
-  return node.value +
-    Math.max(getMaxTreeBranchSum(node.left),
-    getMaxTreeBranchSum(node.right))
-}
