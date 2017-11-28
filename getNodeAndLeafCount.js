f824938cf82bf458164a3aa2bfe1c56247950b6d
matt <mttrys@gmail.com>
2017-11-28 12:30:53 -0800

    chore: rename files. fix require directory. clean comments

--- getNodeAndLeafCount.js
+++ getNodeAndLeafCount.js
@@ -1,12 +1,8 @@
-require('./trees/treeStructures');
-
-
 function getLeafNodeCount(node) { // nodes with no children
   if (!node) return 0;
   if (node.left === null && node.right === null) return 1;
   return getLeafNodeCount(node.left) + getLeafNodeCount(node.right)
 }
-// console.log(getLeafNodeCount(binaryTreeExampleData)) // 4
 
 
 function getNodeCount(node) { // count all the nodes
@@ -16,4 +12,3 @@ function getNodeCount(node) { // count all the nodes
 
 module.exports.getLeafNodeCount = getLeafNodeCount;
 module.exports.getNodeCount = getNodeCount;
-// console.log(GetNodeCount(binaryTreeExampleData)) // 9
---
+++ tests/getNodeAndLeafCount.test.js
@@ -0,0 +1,12 @@
+const binaryTree = require('../trees/treeBinary');
+const getLeafNodeCount = require('../getNodeAndLeafCount').getLeafNodeCount;
+const getNodeCount = require('../getNodeAndLeafCount').getNodeCount;
+
+
+test('Will accurately count the number of leaf nodes', () => {
+  expect(getLeafNodeCount(binaryTree)).toBe(4);
+});
+
+test('Will accurately count the total number of nodes', () => {
+  expect(getNodeCount(binaryTree)).toBe(9);
+});
